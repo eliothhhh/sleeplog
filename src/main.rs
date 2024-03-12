23006be56e9152086cb9c36d2359f67ac206f447
Eliot H <elioth541@gmail.com>
2024-03-12 09:10:39 +0100

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use crate::tag::Tag;
 fn main() {
     let mut j = Journal::new("test");
     let a = Article::new("foo", "abcd", vec![]);
+    
+    println!("{}", a.to_string());
     j.add_article(a);
     println!("{}", j.to_string());
     
